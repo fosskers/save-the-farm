9addec1db947863898f3363d72a99ffd4644845d
Colin Woodbury <colin@fosskers.ca>
2024-09-02 06:09:26 +0900

    [sprites] attempt to load sword guy

--- src/main.lisp
+++ src/main.lisp
@@ -9,20 +9,46 @@
 
 (in-package :save-the-farm)
 
-(setf +app-system+ "save-the-farm")
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (setf +app-system+ "save-the-farm"))
 
 (defclass stf-main (trial-harmony:settings-main)
   ())
 
-;; `base' is somewhat sensitive. If you give it an absolute path it'll believe
-;; it as is, but relative paths seem to be dead-set on staying within `src/' and
-;; don't result in paths relative to the project root.
-(define-pool farm :base #p"/home/colin/code/common-lisp/save-the-farm/data/")
+#+nil
+(uiop:current-lisp-file-pathname)
+
+;; NOTE: `base' is somewhat sensitive. If you give it an absolute path it'll
+;; believe it as is, but relative paths seem to be dead-set on staying within
+;; `src/' and don't result in paths relative to the project root.
+;; (define-pool farm :base #p"../data/")  ; Doesn't work.
+;; (define-pool farm :base #p"data/")  ; Doesn't work.
+;; (define-pool farm :base #p"/home/colin/code/common-lisp/save-the-farm/data/")
 ;; (define-asset (farm tilemap) tile-data #p"map/field.tmj")
+(define-pool farm :root t)
+;; (define-pool farm :base #p"../../data/")
 (define-asset (farm lemon) sprite-data #p"sprites/lemon.json")
+(define-asset (farm guy) sprite-data #p"sprites/sword-guy.json")
+
+#+nil
+(uiop:file-exists-p #p"/home/colin/code/common-lisp/save-the-farm/data/sprites/lemon.json")
+
+#+nil
+(asdf:system-source-directory +app-system+)
+
+#+nil
+(find-pool 'farm)
+
+#+nil
+(pathname-utils:merge-pathnames*
+ "foo"
+ (pathname-utils:subdirectory "/home/colin/code/common-lisp/save-the-farm/src/main.lisp" "data"))
 
 (define-shader-entity my-lemon (animated-sprite located-entity)
   ((sprite-data :initform (asset 'farm 'lemon))))
+
+(define-shader-entity my-guy (animated-sprite located-entity)
+  ((sprite-data :initform (asset 'farm 'guy))))
 
 #+nil
 (find-class 'my-lemon)
@@ -32,15 +58,18 @@
 (define-action shoot (in-game))
 
 (defmethod setup-scene ((main stf-main) scene)
-  ;; (enter (make-instance 'simple-sprite :sprite-data (assets:asset :wolf) :name :sprite) scene)
+  (enter (make-instance 'my-guy :name :guy) scene)
+  ;; NOTE: No need to manually setf the camera slot of the `scene', as an
+  ;; `:after' defmethod on camera+scene already does this.
+  (enter (make-instance 'sidescroll-camera :zoom 5.0 :target (node :guy scene)) scene)
+  ;; (enter (make-instance 'sidescroll-camera :zoom 5.0) scene)
   ;; (enter (make-instance 'tile-layer :tile-data (asset 'farm 'tilemap)) scene)
-  (enter (make-instance 'my-lemon :name :lemon) scene)
+  ;; (enter (make-instance 'my-lemon :name :lemon) scene)
   ;; (enter (make-instance 'sidescroll-camera :zoom 12.0) scene)
-  (enter (make-instance 'sidescroll-camera :zoom 10.0 :target (node :lemon scene)) scene)
   (enter (make-instance 'render-pass) scene))
 
 #+nil
-(find-class 'animated-sprite)
+(maybe-reload-scene)
 
 (defun launch (&rest args)
   "Convenience function for launching the game. Also possible to do any other
