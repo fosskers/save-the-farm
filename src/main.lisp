f4d7ef688b91bc39446f0ec31536fe79b9eddef6
Colin Woodbury <colin@fosskers.ca>
2024-09-03 06:23:10 +0900

    [sprites] they can render!

--- src/main.lisp
+++ src/main.lisp
@@ -9,44 +9,22 @@
 
 (in-package :save-the-farm)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (setf +app-system+ "save-the-farm"))
+(setf +app-system+ "save-the-farm")
 
 (defclass stf-main (trial-harmony:settings-main)
   ())
 
-#+nil
-(uiop:current-lisp-file-pathname)
-
-;; NOTE: `base' is somewhat sensitive. If you give it an absolute path it'll
-;; believe it as is, but relative paths seem to be dead-set on staying within
-;; `src/' and don't result in paths relative to the project root.
-;; (define-pool farm :base #p"../data/")  ; Doesn't work.
-;; (define-pool farm :base #p"data/")  ; Doesn't work.
-;; (define-pool farm :base #p"/home/colin/code/common-lisp/save-the-farm/data/")
-;; (define-asset (farm tilemap) tile-data #p"map/field.tmj")
-(define-pool farm :root t)
-;; (define-pool farm :base #p"../../data/")
+(define-pool farm :base #p"../../data/")
 (define-asset (farm lemon) sprite-data #p"sprites/lemon.json")
 (define-asset (farm guy) sprite-data #p"sprites/sword-guy.json")
 
 #+nil
-(uiop:file-exists-p #p"/home/colin/code/common-lisp/save-the-farm/data/sprites/lemon.json")
-
-#+nil
-(asdf:system-source-directory +app-system+)
-
-#+nil
 (find-pool 'farm)
-
-#+nil
-(pathname-utils:merge-pathnames*
- "foo"
- (pathname-utils:subdirectory "/home/colin/code/common-lisp/save-the-farm/src/main.lisp" "data"))
 
 (define-shader-entity my-lemon (animated-sprite located-entity)
   ((sprite-data :initform (asset 'farm 'lemon))))
 
+#+nil
 (define-shader-entity my-guy (animated-sprite located-entity)
   ((sprite-data :initform (asset 'farm 'guy))))
 
@@ -58,14 +36,12 @@
 (define-action shoot (in-game))
 
 (defmethod setup-scene ((main stf-main) scene)
-  (enter (make-instance 'my-guy :name :guy) scene)
+  ;; (enter (make-instance 'tile-layer :tile-data (asset 'farm 'tilemap)) scene)
+  ;; (enter (make-instance 'my-guy :name :guy) scene)
+  (enter (make-instance 'my-lemon :name :lemon) scene)
   ;; NOTE: No need to manually setf the camera slot of the `scene', as an
   ;; `:after' defmethod on camera+scene already does this.
-  (enter (make-instance 'sidescroll-camera :zoom 5.0 :target (node :guy scene)) scene)
-  ;; (enter (make-instance 'sidescroll-camera :zoom 5.0) scene)
-  ;; (enter (make-instance 'tile-layer :tile-data (asset 'farm 'tilemap)) scene)
-  ;; (enter (make-instance 'my-lemon :name :lemon) scene)
-  ;; (enter (make-instance 'sidescroll-camera :zoom 12.0) scene)
+  (enter (make-instance 'sidescroll-camera :zoom 5.0 :target (node :lemon scene)) scene)
   (enter (make-instance 'render-pass) scene))
 
 #+nil
